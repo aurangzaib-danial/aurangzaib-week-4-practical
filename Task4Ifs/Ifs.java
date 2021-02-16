8583db3dbc02c39b025fed7c48a982ab9c140dbc
Aurangzaib <aurangzaib.danial@gmail.com>
2021-02-16 17:16:45 +0000

    applied teacher feedback

--- Task4Ifs/Ifs.java
+++ Task4Ifs/Ifs.java
@@ -1,23 +1,29 @@
 /**
  * author: Aurangzaib Danial Liaqat Khan
- * version: 13/02/21
+ * version: 16/02/21
  */
 
 public class Ifs {
     public static void main(String[] args) {
-       int userAge = 22;
+       int userAge = 21;
+       String child = "You are a child";
+       String getMarried = "You can get married and have children";
+       String driveCar = "You can drive a car";
+       String vote = "You can vote and drink alcohol";
+
        System.out.println("When the user's age is " + userAge);
+
        if (userAge < 16) {
-           System.out.println("You are a child");
+           System.out.println(child);
        } else if (userAge == 16) {
-           System.out.println("You can get married and have children");
+           System.out.println(getMarried);
        } else if (userAge == 17) {
-           System.out.println("You can get married and have children");
-           System.out.println("You can drive a car");           
-       } else if (userAge == 18) {
-           System.out.println("You can get married and have children");
-           System.out.println("You can drive a car");
-           System.out.println("You can vote and drink alcohol");
+           System.out.println(getMarried);
+           System.out.println(driveCar);           
+       } else if (userAge >= 18 && userAge <= 21) {
+           System.out.println(getMarried);
+           System.out.println(driveCar);
+           System.out.println(vote);
        } else if (userAge > 21) {
            System.out.println("You are not legally inhibited by your age.");
        }
